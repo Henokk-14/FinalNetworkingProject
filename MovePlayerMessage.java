666007b1ca5fa7e4e57decd211a5c2c87a6d081e
Harrison Dominique <54545693+hdominique11@users.noreply.github.com>
2021-04-23 16:24:18 -0400

    Added different type of messages classes.

---
+++ MovePlayerMessage.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+import java.io.Serializable;
+
+public class MovePlayerMessage extends Message implements Serializable {
+    double playerDX;
+    double playerDY;
+    public MovePlayerMessage(double playerDX, double playerDY) {
+
+        this.playerDX= playerDX;
+        this.playerDY = playerDY;
+
+    }
+
+}

d73fa0ac472869d6f932797802ec93a5a75447ad
henokk-14 <henokketsela14@gmail.com>
2021-05-02 18:43:40 -0400

    worked on game state

--- GameState.java
+++ GameState.java
@@ -13,14 +13,17 @@ import java.awt.Color;
 import java.io.PrintStream;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.Point2D;
+import java.io.Serializable;
 
-public class GameState implements Cloneable {
+public class GameState implements Serializable, Cloneable {
+    public static final long serialVersionUID = 340L;
     // Inner class: A simple cell on the board
-    class Cell implements Cloneable {
+    class Cell implements Serializable,Cloneable {
+        public static final long serialVersionUID = 340L;
         double x;  // x position
         double y;  // y position
         double r;  // radius
-        
+
         public Cell(double x, double y, double r) {
             this.x = x;
             this.y = y;
@@ -30,7 +33,7 @@ public class GameState implements Cloneable {
         public Object clone() throws CloneNotSupportedException {
             return super.clone();
         }
-        
+
         /**
          * Determine if two cells collide and have larger absorb smaller cell
          * @param other The other cell to check for collision with this cell
@@ -69,15 +72,16 @@ public class GameState implements Cloneable {
             }
         }
     }
-    
+
     // Inner class: Just a player, with name and their list of cells
-    class Player implements Cloneable {
+    class Player implements Serializable, Cloneable {
+        public static final long serialVersionUID = 340L;
         String name;  // Name to display
         Color appearance;  // The appearance of this player
         ArrayList<Cell> cell; // The various cells associated with this player
         double dx;   // The direction the player is currently moving in
         double dy;
-        
+
         public Player(String n, double initX,  double initY, double initR, Color appearance) {
             this.name = n;
             cell = new ArrayList<>();
@@ -105,14 +109,14 @@ public class GameState implements Cloneable {
             this.dx = dx;
             this.dy = dy;
         }
-        
+
         /**
          * Add a new cell for this player
          **/
         public void addCell(double x, double y, double r) {
             cell.add(new Cell(x, y, r));
         }
-        
+
         /**
          * Move all the cells for this player in the general direction dx, dy by the delta factor
          *   The actual distance moved will depend on the mass of the cell.
@@ -171,7 +175,7 @@ public class GameState implements Cloneable {
             int size = cell.size();
             for (int i = 0; i < size; i++) {
                 if (cell.size() >= maxCells) return;  // No more splits allowed!
-                
+
                 Cell c = cell.get(i);
 
                 // Compute the radius and position of the new cell (and the old one)
@@ -191,7 +195,7 @@ public class GameState implements Cloneable {
                 }
             }
         }
-        
+
         /**
          * Determine any collisions between two groups of Players
          * @param other The other player
@@ -231,7 +235,7 @@ public class GameState implements Cloneable {
             }
             cell = newCell;
         }
-        
+
         /**
          * Generate a string representation of the given player
          * For DEBUGGING purposes mainly
@@ -264,7 +268,7 @@ public class GameState implements Cloneable {
     double minR;   // The smallest that any cell can get (except for "food" particles)
     int maxCells;  // The maximum number of cells allowed for any player
     Random rand;   // Random number generator
-    
+
     public GameState() {
         player = new ArrayList<Player>(2);  // Initial size
         maxX = 1000.0;
@@ -293,14 +297,14 @@ public class GameState implements Cloneable {
     public boolean isDone() {
         return false;  // For now, runs forever!
     }
-    
+
     /**
      * @returns A random Point on the game state
      **/
     public Point2D.Double randomPosition() {
         return new Point2D.Double(rand.nextDouble()*maxX, rand.nextDouble()*maxY);
     }
-    
+
     /**
      * Add a player to the Game State.  All future references to this 
      * player should use the index returned.
@@ -345,8 +349,8 @@ public class GameState implements Cloneable {
     public Player getFood() {
         return food;
     }
-    
-    /** 
+
+    /**
      * Add a piece of random food on the board - anywhere 
      **/
     public void addRandomFood() {
@@ -363,16 +367,17 @@ public class GameState implements Cloneable {
             p.move(delta);
         }
     }
-    
+
     // Get the bounding box of the given player's cells
     public Rectangle2D.Double getBoundingBox(int p) {
         double cellMinX = maxX;
         double cellMaxX = 0;
         double cellMinY = maxY;
         double cellMaxY = 0;
+        System.err.println("GBB: player id = "+p+" number of players are "+player.size());
         Player pl = player.get(p);
         if (pl.cell.size() == 0) return new Rectangle2D.Double(0, 0, maxX, maxY);  // Full screen
-        
+
         for (Cell c: pl.cell) {
             if (c.x-c.r < cellMinX) cellMinX = c.x - c.r;
             if (c.x+c.r > cellMaxX) cellMaxX = c.x + c.r;
@@ -382,7 +387,7 @@ public class GameState implements Cloneable {
 
         return new Rectangle2D.Double(cellMinX, cellMinY, cellMaxX - cellMinX, cellMaxY - cellMinY);
     }
-    
+
     /**
      * Display the "Game State"
      **/
